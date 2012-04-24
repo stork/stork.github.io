aa14fda1aa06619ca794e62aa774937af1eb8505
Lubomir Stork <lubomir.stork@gmail.com>
2012-04-24 17:39:29 +0000

    Merge js to all.js

---
+++ _includes/js/site.js
@@ -0,0 +1,13 @@
+---
+layout: nil
+---
+var poems = [{% for post in site.tags.poem %}{id: '{{ post.slug }}', name: '{{ post.title }}'}{% if forloop.last %}];{% else %},{% endif %}{% endfor %}
+
+$(function() { 
+	$('#search-poems').typeahead({
+		source: poems,
+		itemSelected: function(item, val, text) {
+			window.location.hash = val;
+		}
+	});
+})
