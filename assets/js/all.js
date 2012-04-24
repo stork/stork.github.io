ddebbefa6f934c2126dd58dd192857c7b6e9c6cf
Lubomir Stork <lubomir.stork@gmail.com>
2012-04-24 17:43:54 +0000

    Add whitespace to all.js

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,7 +2,11 @@
 layout: nil
 ---
 {% include js/jquery.min.js %}
+
 {% include js/bootstrap.min.js %}
+
 {% include js/bootstrap-typeahead.min.js %}
+
 {% include js/prettify.js %}
+
 {% include js/site.js %}
