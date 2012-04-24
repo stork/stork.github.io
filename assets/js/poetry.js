4cbe9b5760a9e2b91dc011755d7acc7e312e8de6
Lubomir Stork <lubomir.stork@gmail.com>
2012-04-24 17:20:31 +0000

    Tweak poetry.js

--- assets/js/poetry.js
+++ assets/js/poetry.js
@@ -1,8 +1,6 @@
 ---
 layout: nil
 ---
-var poems = [
-{% for post in site.tags.poem %}
-	{id: '{{ post.slug }}', name: '{{ post.title }}'}{% if forloop.rindex %},{% endif %}
-{% endfor %}
-];
+var poems = [{% for post in site.tags.poem %}
+	{id: '{{ post.slug }}', name: '{{ post.title }}'}{% if forloop.rindex0 %},{% endif %}
+{% endfor %}];
