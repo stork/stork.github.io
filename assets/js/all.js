861c52db43cba95d7a382c657dcfbffb92c583d4
Lubomir Stork <lubomir.stork@gmail.com>
2012-04-24 17:48:27 +0000

    Remove whitespace

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,11 +2,7 @@
 layout: nil
 ---
 {% include js/jquery.min.js %}
-
 {% include js/bootstrap.min.js %}
-
 {% include js/bootstrap-typeahead.min.js %}
-
 {% include js/prettify.js %}
-
 {% include js/site.js %}
