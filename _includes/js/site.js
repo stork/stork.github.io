bbae4eeaf200ff41cb4776f2742c5730c9fee976
Lubomir Stork <lubomir.stork@gmail.com>
2012-04-24 17:40:52 +0000

    Tweak site.js

--- _includes/js/site.js
+++ _includes/js/site.js
@@ -1,8 +1,4 @@
----
-layout: nil
----
-var poems = [{% for post in site.tags.poem %}{id: '{{ post.slug }}', name: '{{ post.title }}'}{% if forloop.last %}];{% else %},{% endif %}{% endfor %}
-
+var poems = [{% for post in site.tags.poem %}{id:'{{ post.slug }}',name:'{{ post.title }}'}{% if forloop.last %}];{% else %},{% endif %}{% endfor %}
 $(function() { 
 	$('#search-poems').typeahead({
 		source: poems,
