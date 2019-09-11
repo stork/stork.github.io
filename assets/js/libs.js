6f57f99d28c30220f5b5640cb22bb5b33c38fee0
Lubomír Štork <lubomir.stork@gmail.com>
2019-09-11 08:04:37 +0200

    cleanup

--- assets/js/libs.js
+++ assets/js/libs.js
@@ -4,9 +4,3 @@ layout: nil
 {% include js/jquery.min.js %}
 {% include js/bootstrap.min.js %}
 {% include js/bootstrap-typeahead.min.js %}
-{% include js/prettify.js %}
-{% include js/fixie_min.js %}
-{% include js/sammy-latest.min.js %}
-{% include js/sammy.googleanalytics-latest.min.js %}
-{% include js/sammy.storage-latest.min.js %}
-{% include js/sammy.title-latest.min.js %}
