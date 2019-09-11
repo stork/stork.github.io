0767a316d5fb9b698be0b746fae0efd9deb479f8
Lubomír Štork <lubomir.stork@gmail.com>
2019-09-11 07:35:41 +0200

    typeahead cleanup

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,26 +2,12 @@
 layout: nil
 ---
 var poems = [{% for post in site.tags.poem %}{id:'{{ post.slug }}',name:'{{ post.title }}'}{% if forloop.last %}];{% else %},{% endif %}{% endfor %}
-
-
-var poemsData = [{% for post in site.tags.poem %}{id:'{{ post.slug }}',name:'{{ post.content }}'}{% if forloop.last %}];{% else %},{% endif %}{% endfor %}
-
-
 $(function() {
 	$('#search-poems').typeahead({
 		source: poems,
 		items: 16,
 		itemSelected: function(item, val, text) {
 			_gaq.push(['_trackEvent', 'Poetry', 'Search', text]);
-			window.location.hash = val;
-		}
-	});
-
-	$('#search-poems-data').typeahead({
-		source: poemsData,
-		items: 16,
-		itemSelected: function(item, val, text) {
-//			_gaq.push(['_trackEvent', 'Poetry', 'Search', text]);
 			window.location.hash = val;
 		}
 	});
