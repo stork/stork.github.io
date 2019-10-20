04747ae802ab1a9161ed91cd5bdd017d3d2a7a74
Lubomir Stork <lubomir.stork@gmail.com>
2019-10-20 01:43:43 +0100

    debug events

--- _includes/js/app/controller/container.js
+++ _includes/js/app/controller/container.js
@@ -7,43 +7,6 @@ $(function() {
 			this.log('SAMMY not found');
 		};
 
-		this.bind('run', function() {
-			Sammy.log('EVENT run');
-		});
-		this.bind('load', function() {
-			Sammy.log('EVENT load');
-		});
-		this.bind('lookup-route', function() {
-			this.log('EVENT lookup-route');
-		});
-		this.bind('run-route', function() {
-			this.log('EVENT run-route');
-		});
-		this.bind('route-found', function() {
-			this.log('EVENT route-found');
-		});
-		this.bind('event-context-before', function() {
-			this.log('EVENT event-context-before');
-		});
-		this.bind('event-context-after', function() {
-			this.log('EVENT event-context-after');
-		});
-		this.bind('changed', function() {
-			this.log('EVENT changed');
-		});
-		this.bind('error', function() {
-			this.log('EVENT error');
-		});
-		this.bind('check-form-submission', function() {
-			this.log('EVENT check-form-submission');
-		});
-		this.bind('redirect', function() {
-			this.log('EVENT redirect');
-		});
-		this.bind('location-changed', function() {
-			this.log('EVENT location-changed');
-		});
-
 		this.get('/', function() {
 			this.log('SITE home');
 		});
@@ -51,14 +14,14 @@ $(function() {
 		this.get('#/', function() {
 			this.log('SAMMY home');
 		});
-/*
+
 		this.get('#:id', function() {
 			this.log('POEM NAMED ' + this.params.id);
 		});
-*/
+/*
 		this.get(/\#(.*)/, function() {
 			this.log('POEM REGEX ' + this.params['splat']);
 		});
-
+*/
 	}).run();
 });
