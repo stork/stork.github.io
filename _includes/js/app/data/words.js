ad6a02f3ffbd40c1756cb7d82ec743532dfb525f
Lubomir Stork <lubomir.stork@gmail.com>
2019-10-21 00:47:33 +0100

    jsonify test

--- _includes/js/app/data/words.js
+++ _includes/js/app/data/words.js
@@ -14,6 +14,16 @@
 		{% assign tmp = tmp | remove: '“' %}
 		{% assign tmp = tmp | remove: "'" %}
 		{% assign tmp = tmp | remove: '`' %}
+		{% assign tmp = tmp | remove: '0' %}
+		{% assign tmp = tmp | remove: '1' %}
+		{% assign tmp = tmp | remove: '2' %}
+		{% assign tmp = tmp | remove: '3 %}
+		{% assign tmp = tmp | remove: '4' %}
+		{% assign tmp = tmp | remove: '5' %}
+		{% assign tmp = tmp | remove: '6' %}
+		{% assign tmp = tmp | remove: '7' %}
+		{% assign tmp = tmp | remove: '8' %}
+		{% assign tmp = tmp | remove: '9' %}
 		{% assign thesize = tmp | size %}
 		{% if thesize > 0 %}
 			{% assign tmp = tmp | downcase %}
