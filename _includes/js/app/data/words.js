bf4e058107dc46eaba35688469a108059774ffb5
Lubomir Stork <lubomir.stork@gmail.com>
2019-10-21 00:14:43 +0100

    jsonify test

--- _includes/js/app/data/words.js
+++ _includes/js/app/data/words.js
@@ -2,6 +2,7 @@
 {% for post in site.tags.poem %}
 	{% assign postwords = post.content | split:' ' %}
 	{% for word in postwords %}
+		{% assign word = word | remove: '.' | remove: ',' | remove: '"' | remove: '(' | remove: ')' | remove: ';' | remove: '-' | remove: '!' | remove: '?' %}
 		{% assign words = words | push: word %}
 	{% endfor %}
 {% endfor %}
