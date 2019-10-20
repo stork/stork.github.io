6b4591f58e7039b28d7e1757858e11b97e134e7c
Lubomir Stork <lubomir.stork@gmail.com>
2019-10-21 00:31:48 +0100

    jsonify test

--- _includes/js/app/data/words.js
+++ _includes/js/app/data/words.js
@@ -2,17 +2,17 @@
 {% for post in site.tags.poem %}
 	{% assign postwords = post.content | split: ' ' %}
 	{% for word in postwords %}
-		{% assign word = word | remove: '.' %}
-		{% assign word = word | remove: ',' %}
-		{% assign word = word | remove: '"' %}
-		{% assign word = word | remove: '(' %}
-		{% assign word = word | remove: ')' %}
-		{% assign word = word | remove: ';' %}
-		{% assign word = word | remove: '-' %}
-		{% assign word = word | remove: '!' %}
-		{% assign word = word | remove: '?' %}
-		{% assign word = word | downcase %}
-		{% assign words = words | push: word %}
+		{% assign tmp = word | remove: '.' %}
+		{% assign tmp = tmp | remove: ',' %}
+		{% assign tmp = tmp | remove: '"' %}
+		{% assign tmp = tmp | remove: '(' %}
+		{% assign tmp = tmp | remove: ')' %}
+		{% assign tmp = tmp | remove: ';' %}
+		{% assign tmp = tmp | remove: '-' %}
+		{% assign tmp = tmp | remove: '!' %}
+		{% assign tmp = tmp | remove: '?' %}
+		{% assign tmp = tmp | downcase %}
+		{% assign words = words | push: tmp %}
 	{% endfor %}
 {% endfor %}
 {% assign words = words | uniq %}
