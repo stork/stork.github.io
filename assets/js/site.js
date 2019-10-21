116eb80327462173957635375d204ea1214a9c5a
Lubomir Stork <lubomir.stork@gmail.com>
2019-10-21 01:11:02 +0100

    jsonify test

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,6 @@
 ---
 layout: nil
 ---
-{% include js/app/data/poems.js %}
 {% include js/app/search.js %}
 {% include js/app/model/behavior/paginated.js %}
 {% include js/app/model/poem.js %}
@@ -9,4 +8,3 @@ layout: nil
 {% include js/app/controller/container.js %}
 {% include js/app/controller/pagination.js %}
 {% include js/app/controller/well.js %}
-{% include js/app/data/words.js %}
---
+++ assets/js/words.js
@@ -0,0 +1,37 @@
+{% assign words = '' | split: ' ' %}
+{% for post in site.tags.poem %}
+{% assign postwords = post.content | split: ' ' %}
+{% for word in postwords %}
+{% assign tmp = word | remove: '.' %}
+{% assign tmp = tmp | remove: ',' %}
+{% assign tmp = tmp | remove: '"' %}
+{% assign tmp = tmp | remove: '(' %}
+{% assign tmp = tmp | remove: ')' %}
+{% assign tmp = tmp | remove: ';' %}
+{% assign tmp = tmp | remove: '-' %}
+{% assign tmp = tmp | remove: '!' %}
+{% assign tmp = tmp | remove: '?' %}
+{% assign tmp = tmp | remove: '“' %}
+{% assign tmp = tmp | remove: '”' %}
+{% assign tmp = tmp | remove: "'" %}
+{% assign tmp = tmp | remove: '`' %}
+{% assign tmp = tmp | remove: '0' %}
+{% assign tmp = tmp | remove: '1' %}
+{% assign tmp = tmp | remove: '2' %}
+{% assign tmp = tmp | remove: '3' %}
+{% assign tmp = tmp | remove: '4' %}
+{% assign tmp = tmp | remove: '5' %}
+{% assign tmp = tmp | remove: '6' %}
+{% assign tmp = tmp | remove: '7' %}
+{% assign tmp = tmp | remove: '8' %}
+{% assign tmp = tmp | remove: '9' %}
+{% assign thesize = tmp | size %}
+{% if thesize > 0 %}
+{% assign tmp = tmp | downcase %}
+{% assign words = words | push: tmp %}
+{% endif %}
+{% endfor %}
+{% endfor %}
+{% assign words = words | uniq %}
+{% assign words = words | sort %}
+var words = {{ words | jsonify }};
