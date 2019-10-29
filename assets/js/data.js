45288c22977f46a77ceb7bcd6415954c1ee54e1f
Lubomir Stork <lubomir.stork@gmail.com>
2019-10-29 04:03:58 +0000

    shortened raw data output

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,32 +1,13 @@
 ---
 	layout: nil
 ---
-
-var poemsFull = {{ site.tags.poem | jsonify }};
-
-
 {% assign poemIds = '' | split: '' %}
 {% assign poemNames = '' | split: '' %}
 {% assign poemBodies = '' | split: '' %}
-
 {% for post in site.tags.poem %}
 	{% assign poemIds = poemIds | push: post.slug %}
 	{% assign poemNames = poemNames | push: post.title %}
 	{% assign poemBodies = poemBodies | push: post.content %}
 {% endfor %}
 
-
-
-var poemsIds = {{ poemIds | jsonify }};
-
-
-
-
-
-var poemsNames = {{ poemNames | jsonify }};
-
-
-
-
-
-var poemsBodies = {{ poemBodies | jsonify }};
+var poemsData = {ids:{{ poemIds | jsonify }},names:{{ poemNames | jsonify }},bodies:{{ poemBodies | jsonify }}};
